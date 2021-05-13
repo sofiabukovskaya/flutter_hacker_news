50cd35a135465f54a3c405597c4f8f41b72ac302
sofiabukovskaya <sofi454545@gmail.com>
2021-05-13 16:07:59 +0300

    Created DB "Items" and table columns

---
+++ lib/domain/repository/news_db_provider.dart
@@ -0,0 +1,35 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+
+class NewsDbProvider {
+  Database database;
+
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'items.db');
+
+    database = await openDatabase(path,
+        version: 1, onCreate: (Database newDb, int version) {
+            newDb.execute('''
+              CREATE TABLE Items
+              (
+                id INTEGER PRIMARY KEY,
+                type TEXT,
+                by TEXT,
+                time INTEGER,
+                text TEXT,
+                parent INTEGER, 
+                kids BLOB,
+                dead  INTEGER,
+                deleted INTEGER, 
+                url TEXT,
+                score INTEGER,
+                title TEXT,
+                descendants INTEGER
+              )
+            ''');
+        });
+  }
+}
